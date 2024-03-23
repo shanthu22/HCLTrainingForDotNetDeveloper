33ae538495b4f18306e3723771d9678d92451f60
Shanthu Shanthu <ssanthujan@yahoo.com>
2024-03-23 16:24:56 +0530

    first commit

---
+++ myproject1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myproject1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int num;
+            Console.WriteLine("Enter your name ");
+            name= Console.ReadLine();
+            
+            Console.WriteLine(name);
+
+            Console.WriteLine("Enter a number ");
+            num= int.Parse(Console.ReadLine());
+            Console.WriteLine(num+ num);
+
+            Program.MyDateCheck();
+
+
+        }
+
+        static string MyDateCheck() {
+            Console.WriteLine("Welcome to Date CHeck Method ");
+            DateTime d1 = DateTime.Now;
+            Console.WriteLine(d1.ToShortDateString());
+            Console.WriteLine(d1.Year);
+
+            return "initialTest";
+        }
+    }
+}
