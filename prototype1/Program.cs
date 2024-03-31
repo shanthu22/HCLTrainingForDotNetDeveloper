b5291dd4482dbbde09a5fbd8f2b62c83f5a22de1
Shanthu Shanthu <ssanthujan@yahoo.com>
2024-04-01 01:49:22 +0530

    prototype pattern

---
+++ prototype1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prototype1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Im called : Prototype");
+            PrototypeA obj = new PrototypeA("Check it ");
+            
+            Console.WriteLine(obj.getField());
+
+            var cloneObj = obj.Clone() as PrototypeA;
+
+          
+            cloneObj.setField("--Clone--");
+            Console.WriteLine("my clone obj is " + cloneObj.getField());
+
+        }
+    }
+
+    interface iprototype {
+        iprototype Clone();
+    }
+
+    class PrototypeA : iprototype
+    {
+        private string FieldA;
+
+        public PrototypeA(string _FieldA) {
+            FieldA=  _FieldA;
+        }
+        public void setField(string _FieldA) { 
+        this.FieldA= _FieldA;
+        }
+
+        public string getField() {
+
+            return FieldA;
+
+        }
+        public iprototype Clone() { 
+        return new PrototypeA(FieldA);
+        }
+
+
+    }
+
+}
